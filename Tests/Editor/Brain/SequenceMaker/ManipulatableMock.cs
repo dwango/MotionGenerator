98df0cc9c238ca58f2a9760243fc7af536ec60c2
Keisuke-Ogaki <keisuke_ogaki@dwango.co.jp>
2018-05-31 18:51:11 +0900

    :art: reduce dependency: test

--- Tests/Editor/Brain/SequenceMaker/ManipulatableMock.cs
+++ Tests/Editor/Brain/SequenceMaker/ManipulatableMock.cs
@@ -1,47 +1,10 @@
-﻿using BodyGenerator.Body.Manipulatables;
-
-namespace MotionGenerator
+﻿namespace MotionGenerator
 {
-    public class ManipulatableMock : IManipulatable
+    public class ManipulatableMock
     {
-        public void Init(ITime time, IScanner scanner, ITileScanner tileScanner, IGenealogy genealogy)
-        {
-        }
-
-        public void SetManipulatableId(int value)
-        {
-        }
-
         public virtual int GetManipulatableDimention()
         {
             return 3;
-        }
-
-        public int GetManipulatableId()
-        {
-            return 0;
-        }
-
-        public bool IsMoving()
-        {
-            return false;
-        }
-
-        public void Manipulate(MotionSequence sequence)
-        {
-        }
-
-        public State GetState()
-        {
-            return new State();
-        }
-
-        public void UpdateFixedFrame()
-        {
-        }
-
-        public void ResetFromPrefab()
-        {
         }
     }
 
