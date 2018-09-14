084d26b775107404999cdffef8d4537d54a15436
CHINO yuji <chino@dwango.co.jp>
2018-09-14 15:38:12 +0900

    enable LZ4

--- Scripts/Serialization/MotionGeneratorSerialization.cs
+++ Scripts/Serialization/MotionGeneratorSerialization.cs
@@ -6,17 +6,27 @@ namespace MotionGenerator.Serialization
     {
         public static byte[] Serialize<T>(T obj)
         {
-            return MessagePackSerializer.Serialize(obj);
+            return LZ4MessagePackSerializer.Serialize(obj);
         }
 
         public static T Deserialize<T>(byte[] data)
         {
-            return MessagePackSerializer.Deserialize<T>(data);
+            return LZ4MessagePackSerializer.Deserialize<T>(data);
         }
 
         public static T DeepClone<T>(T obj)
         {
-            return Deserialize<T>(Serialize(obj));
+            return DeserializeFast<T>(SerializeFast(obj));
+        }
+
+        private static byte[] SerializeFast<T>(T obj)
+        {
+            return MessagePackSerializer.Serialize(obj);
+        }
+
+        private static T DeserializeFast<T>(byte[] data)
+        {
+            return MessagePackSerializer.Deserialize<T>(data);
         }
     }
 }
