23cd972ee61a6240b476518b529aa014a7ff37ae
CHINO yuji <chino@dwango.co.jp>
2018-10-31 20:22:09 +0900

    remove LZ4 Serialization

--- Scripts/Serialization/MotionGeneratorSerialization.cs
+++ Scripts/Serialization/MotionGeneratorSerialization.cs
@@ -6,27 +6,17 @@ namespace MotionGenerator.Serialization
     {
         public static byte[] Serialize<T>(T obj)
         {
-            return LZ4MessagePackSerializer.Serialize(obj);
+            return MessagePackSerializer.Serialize(obj);
         }
 
         public static T Deserialize<T>(byte[] data)
         {
-            return LZ4MessagePackSerializer.Deserialize<T>(data);
+            return MessagePackSerializer.Deserialize<T>(data);
         }
 
         public static T DeepClone<T>(T obj)
         {
-            return DeserializeFast<T>(SerializeFast(obj));
-        }
-
-        private static byte[] SerializeFast<T>(T obj)
-        {
-            return MessagePackSerializer.Serialize(obj);
-        }
-
-        private static T DeserializeFast<T>(byte[] data)
-        {
-            return MessagePackSerializer.Deserialize<T>(data);
+            return Deserialize<T>(Serialize(obj));
         }
     }
 }
